20c3d4115f30c47468103f3448a4fca106d78569
NullPlace1 <soysoloalguienxd@gmail.com>
2025-07-13 13:30:20 -0500

    Alfin el JS tiene algo que hacer
    No fue complicado nada mas me demore 20 minutos en buscar como hacer esto

---
+++ VScode/Extras.js
@@ -0,0 +1,5 @@
+document.getElementById("PizzaSpeed").addEventListener("click", function () {
+  document.getElementById("speedR").textContent = "🔥--------------------------🍕👍🏘️";
+});
+
+//POR FIN ALGO XD

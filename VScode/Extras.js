ba053fc5efed99de3b91695f77395aea63163769
NullPlace1 <soysoloalguienxd@gmail.com>
2025-07-14 16:49:34 -0500

    Xosdadasdas

--- VScode/Extras.js
+++ VScode/Extras.js
@@ -3,15 +3,11 @@ document.getElementById("PizzaSpeed").addEventListener("click", function () {
   const speedElement = document.getElementById("speedR");
   const originalText = speedElement.textContent;
 
-  // Cambiar el texto y desactivar el botón
   speedElement.textContent = "🔥--------------------------🍕👍🏘️";
   button.disabled = true;
 
-  // Revertir el texto y reactivar el botón después de 1 segundo
-  setTimeout(function () {
+  setTimeout(() => {
     speedElement.textContent = originalText;
     button.disabled = false;
-  }, 1000);
+  }, 1100);
 });
-
-// mas cosas ya no es inservible del todo xd
